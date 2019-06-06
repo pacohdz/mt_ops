f59ee390ac69ad143644ea7580e4b834a4f91001
Francisco  Hernández <juan.hernandez.garza@tuhh.de>
2019-06-06 15:48:38 +0200

    7K

--- multiple_sim_runs_temp_hack.py
+++ multiple_sim_runs_temp_hack.py
@@ -6,7 +6,7 @@ from subprocess import call
 import datetime
 
 def main():
-    runs = 20000
+    runs = 7000
     for x in range (runs):
         modify_seed(x)
         call(["./ops-simu-run.sh", "-m", "cmdenv"])
