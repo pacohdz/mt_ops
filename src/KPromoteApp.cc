6c8b6dbc6c906f70ca10315840e50108743f92a4
Francisco  Hernández <juan.hernandez.garza@tuhh.de>
2019-06-12 22:33:52 +0200

    10K CR 3.5m walking

--- src/KPromoteApp.cc
+++ src/KPromoteApp.cc
@@ -52,7 +52,7 @@ void KPromoteApp::initialize(int stage)
         if (dataGeneratingNodeIndex == 0){
             //dataGeneratingNodeIndex = rnd_int; // For complete random scenarios
             dataGeneratingNodeIndex = 0; // Node 0 generates the initial data
-            //EV_INFO << "Node: " << rnd_int << " generates Information \n"; ####
+            //EV_INFO << "Node: " << rnd_int << " generates Information \n";
         }
   
     } else if (stage == 1) {
