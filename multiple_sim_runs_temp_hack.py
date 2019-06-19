ae71c4d7317f37545e67f393c903b1e8af37294a
Francisco  Hernández <juan.hernandez.garza@tuhh.de>
2019-06-19 14:39:44 +0200

    FINAL Scenario 1 Walking CR 2.5m 10K

--- multiple_sim_runs_temp_hack.py
+++ multiple_sim_runs_temp_hack.py
@@ -6,7 +6,7 @@ from subprocess import call
 import datetime
 
 def main():
-    runs = 5000
+    runs = 10000
     for x in range (0, runs):
         modify_seed(x)
         call(["./ops-simu-run.sh", "-m", "cmdenv"])
