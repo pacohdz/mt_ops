5dd6fdd4770b1cd5a5286bc7a9f561b56516b61c
Francisco  Hernández <juan.hernandez.garza@tuhh.de>
2019-07-01 10:39:15 +0200

    FINAL Scenario 1 CR 2.5m NO MOBILITY EXTRA 600

--- multiple_sim_runs_temp_hack.py
+++ multiple_sim_runs_temp_hack.py
@@ -7,7 +7,7 @@ import datetime
 
 def main():
     runs = 600
-    for x in range (1201, 1201+runs):
+    for x in range (1801, 1801+runs):
         modify_seed(x)
         if (x != 0) and (x != 1) and (x != 12) and (x != 13) and (x != 665):  # Skip problematic seeds
             call(["./ops-simu-run.sh", "-m", "cmdenv"])
@@ -24,7 +24,7 @@ def modify_seed(seed):
         filedata = file.read()
 
     # Replace the target string
-    if seed == 1201:
+    if seed == 1801:
         filedata = filedata.replace('seed-0-mt = ${{repetition}}'.format(42), 'seed-0-mt = ' + str(seed) )
     else:
         filedata = filedata.replace('seed-0-mt = ' + str((seed - 1)), 'seed-0-mt = ' + str(seed) )
